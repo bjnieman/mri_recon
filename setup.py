6a0123a44a17b1a95c20583014cc6570b198f668
aesdeguzman <aesdeguzman@gmail.com>
2017-07-05 10:02:45 -0400

    Change directory structure and add code to read zipped modules: - Python doesn't like it when the directory and runnable script have the same name. - setup.py file installs as a zipped .egg file 	- the modules inside the .egg file can not be read by the traditional loader 	- added a check for zip file so that approproate module loaded can be used

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 import sys
 
-setup(name="mri_recon",
+setup(name="mri_python",
       version='0.12',
       install_requires=[
         'ConfigArgParse>=0.11',
@@ -12,7 +12,8 @@ setup(name="mri_recon",
         'pyminc',
         'typing',
       ],
-      packages=['mri_recon'],
-      scripts=['mri_recon/mri_recon.py'
+      #zip_safe= False,
+      packages=['mri_python'],
+      scripts=['mri_python/mri_recon.py'
                ],
       )
